bdd21d4d88f0d6a5cf1afa4628d3ae054b88cc6b
deviaaprli <127111251+deviaaprli@users.noreply.github.com>
2023-04-03 17:29:14 +0700

    Update ExampleStack.java

--- modul3/src/latihan1/ExampleStack.java
+++ modul3/src/latihan1/ExampleStack.java
@@ -3,15 +3,67 @@ package latihan1;
 import java.util.Stack;
 
 public class ExampleStack {
+    private int maxSize;
+    private int top;
+    private String[] stackArray;
+
+    public ExampleStack(int size) {
+        maxSize = size;
+        top = -1;
+        stackArray = new String[maxSize];
+    }
+
+    public void push(String value) {
+        if (top < maxSize - 1) {
+            top++;
+            stackArray[top] = value;
+        } else {
+            System.out.println("Stack overflow");
+        }
+    }
+
+    public String pop() {
+        if (top >= 0) {
+            String value = stackArray[top];
+            top--;
+            return value;
+        } else {
+            System.out.println("Stack underflow");
+            return "";
+        }
+    }
+
+    public String peek() {
+        if (top >= 0) {
+            return stackArray[top];
+        } else {
+            System.out.println("Stack is empty");
+            return "";
+        }
+    }
+
+    public boolean empty() {
+        return top == -1;
+    }
+
+    public int search(String value) {
+        for (int i = top; i >= 0; i--) {
+            if (stackArray[i].equals(value)) {
+                return top - i + 1;
+            }
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
-	    Stack st = new Stack<>();
+        Stack st = new Stack();
 
         st.push("Aku");
         st.push("Anak");
         st.push("Indonesia");
 
-        System.out.println("Next : " +st.peek());
+        System.out.println("Next : "+st.peek());
         st.push("Raya");
         System.out.println(st.pop());
         st.push("!");
